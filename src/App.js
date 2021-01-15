4c2e1e3ab4d593b65a3f80a62c3f4370a0dd1c4f
Nazarov Sergey <Sergey.Nazarov@icl-services.com>
2021-01-15 13:51:32 +0300

    Init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import 'tlib/dist/index.css';
+import './App.css';
+import { Title, Button } from 'tlib';
+
+function App() {
+  return (
+    <div className="App">
+      <Title>Test</Title>
+        <Button primary>Test btn</Button>
+    </div>
+  );
+}
+
+export default App;
